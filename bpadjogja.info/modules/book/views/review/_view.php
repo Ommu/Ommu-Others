6f0beca97c1707661fd92fb805759c7be0894911
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-05-15 07:06:15 +0700

    change 't' to 'slug'

--- bpadjogja.info/modules/book/views/review/_view.php
+++ bpadjogja.info/modules/book/views/review/_view.php
@@ -16,10 +16,10 @@
 ?>
 
 <div class="sep">
-	<a class="images" href="<?php echo Yii::app()->controller->createUrl('view', array('id'=>$data->review_id,'t'=>Utility::getUrlTitle($data->book_relation->title)));?>" title="<?php echo $data->book_relation->title;?>">
+	<a class="images" href="<?php echo Yii::app()->controller->createUrl('view', array('id'=>$data->review_id,'slug'=>Utility::getUrlTitle($data->book_relation->title)));?>" title="<?php echo $data->book_relation->title;?>">
 		<img src="<?php echo Utility::getTimThumb($images, 300, 150, 1);?>" alt="<?php echo $data->book_relation->title;?>" />
 	</a>
-	<a class="title" href="<?php echo Yii::app()->controller->createUrl('view', array('id'=>$data->review_id,'t'=>Utility::getUrlTitle($data->book_relation->title)));?>" title="<?php echo $data->book_relation->title;?>"><?php echo Utility::shortText(Utility::hardDecode($data->book_relation->title),40);?></a>
+	<a class="title" href="<?php echo Yii::app()->controller->createUrl('view', array('id'=>$data->review_id,'slug'=>Utility::getUrlTitle($data->book_relation->title)));?>" title="<?php echo $data->book_relation->title;?>"><?php echo Utility::shortText(Utility::hardDecode($data->book_relation->title),40);?></a>
 	<div class="meta-date clearfix">
 		<span class="date"><i class="fa fa-calendar"></i>&nbsp;<?php echo Utility::dateFormat($data->published_date, true);?></span>
 		<span class="view"><i class="fa fa-eye"></i>&nbsp;<?php echo $data->view;?></span>
